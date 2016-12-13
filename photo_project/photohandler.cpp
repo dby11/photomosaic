085ecb7207c46c5c2e70e2bbb489b804342e5276
DESKTOP-ECV0T4E\DerekY <derekyen0811@g.ucla.edu>
2016-12-13 01:06:15 -0800

    Outlined algorithm steps

--- photo_project/photohandler.cpp
+++ photo_project/photohandler.cpp
@@ -3,3 +3,14 @@
 PhotoHandler::PhotoHandler(QString i, QString d): database(d), imageName(i) {}
 
 
+QImage construct_mosaic() {
+    /*Basic algorithm outline
+     *1. Get list of fileNames
+     *2. Find average RGB values for fileNames in database
+     *3. Read in image
+     *
+     *4. Get average RGB values for each subimage in the image
+     *5. Find best match within the photo database for each image based on avg RGB
+     *6. Assemble mosaic
+    */
+}
--- photo_project/photohandler.h
+++ photo_project/photohandler.h
@@ -1,14 +1,20 @@
 #ifndef PHOTOHANDLER_H
 #define PHOTOHANDLER_H
 
+#include <QImage>
+#include <QString>
 
 class PhotoHandler
 {
 public:
     PhotoHandler(QString i, QString d);
+    QImage construct_mosaic();
+
 private:
     QString database;
     QString imageName;
+    QImage image;
+    std::vector<QString> fileNames;
 };
 
 #endif // PHOTOHANDLER_H
