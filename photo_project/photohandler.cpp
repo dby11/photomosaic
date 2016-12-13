ae28ce60f17ad8e257be72db8d9033526379158e
DESKTOP-ECV0T4E\DerekY <derekyen0811@g.ucla.edu>
2016-12-13 01:20:18 -0800

    avgpixel

--- photo_project/photohandler.cpp
+++ photo_project/photohandler.cpp
@@ -2,6 +2,30 @@
 
 PhotoHandler::PhotoHandler(QString i, QString d): database(d), imageName(i) {}
 
+QColor avgpixel(QImage& img, int wstart, int wend, int hstart, int hend)
+{
+    int total_red = 0;
+    int total_green = 0;
+    int total_blue = 0;
+    int wid = wend - wstart;
+    int ht = hend - hstart;
+    for (int row = hstart; row < hend; ++row)
+    {
+        for(int col = wstart; col <wend; ++col)
+        {
+            QColor pixels = img.pixel(col,row);
+            total_red += pixels.red();
+            total_green += pixels.green();
+            total_blue += pixels.blue();
+        }
+    }
+    int avg_red = total_red/(wid*ht);
+    int avg_blue = total_blue/(wid*ht);
+    int avg_green = total_green/(wid*ht);
+
+    QColor avg_rgb(avg_red, avg_blue, avg_green);
+    return avg_rgb;
+}
 
 QString search_database(std::vector<QColor> db_pixels, QColor colors)
 {
